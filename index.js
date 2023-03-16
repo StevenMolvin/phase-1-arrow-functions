1110731bbc2bd5e730fc6deffa90707cf78678df
Stephen <stevenmolvin@gmail.com>
2023-03-16 05:46:17 +0300

    Completed asignment

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// index.js
-const divide = function () {
+let a = 100;
+let b = 2000;
+let c = 8;
+let d = 9;
+let x = 7;
+
+const divide = function (a, b) {
   return 2000 / 100;
 };
 const square = (x) => {
+  
   return x * x;
 };
 
-const add = (a, b) => {
-  return a + b;
+const add = (c, d) => {
+  return c + d;
 };
-
